b103c3b79824329be6f9a8ed6d59b507b9946325
Vinay <vinay.1ag@gmail.com>
2020-04-30 03:30:43 +0530

    Added new Controller

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace sample_Web_API.Controllers
+{
+    
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("api/students")]
+        public bool CreateStudent(Student student)
+        {
+            return true;
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace sample_Web_API.Controllers
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string USN { get; set; }
+        public string Branch { get; set; }
+        public string Year { get; set; }
+    }
+}
